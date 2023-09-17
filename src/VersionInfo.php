f4d3172e8f73b855d89d23c71afcf4d5abd9cd61
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-17 15:50:58 +0700

    Update VersionInfo.php

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -1,35 +1,16 @@
 <?php
 
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- *
- *
-*/
-
 declare(strict_types=1);
 
-namespace pocketmine;
+namespace PocketFuzy;
 
-use pocketmine\utils\Git;
-use pocketmine\utils\VersionString;
+use PocketFuzy\utils\Git;
+use PocketFuzy\utils\VersionString;
 use function str_repeat;
 
 final class VersionInfo{
-	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "4.0.0";
+	public const NAME = "PocketFuzy";
+	public const BASE_VERSION = "5.0.0";
 	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_NUMBER = 0;
 
